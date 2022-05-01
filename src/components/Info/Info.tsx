ab2e6fef6e847869385ef6124b3dbbdfb097c644
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2022-05-01 13:54:39 +0300

    Add  accordion to the Q&A section

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,89 +1,181 @@
-import { Typography, Divider } from "@mui/material";
+import { useState, SyntheticEvent } from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 
-const Info = () => (
-    <>
-        <Divider />
-        <Typography variant="h4" component="h2" gutterBottom sx={{ textAlign: 'center', mt: 2 }}>
-            Питання та Відповіді
-        </Typography>
+const Accordion = styled((props: AccordionProps) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&:before': {
+        display: 'none',
+    },
+}));
 
-        {/* {What is} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Що таке "autoRia Analyser?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Аналізатор - це інструмен, який візуально відображає авторинок України в реальному часі.
-        </Typography>
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    backgroundColor:
+        theme.palette.mode === 'dark'
+            ? 'rgba(255, 255, 255, .05)'
+            : 'rgba(0, 0, 0, .03)',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: theme.spacing(1),
+    },
+}));
 
-        {/* {What for} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Для чого потрібен аналізатор?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Щоб в пару кліків зрозуміти, яке найліпше авто можна придбати в Україні, відштовхуючись від бюджету і побажань.
-        </Typography>
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
 
-        {/* {How does it work} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Як працює аналізатор?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Аналізатор шукає оголошення на autoRia.ua, збирає статистику і будує інформативні графіки.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Для початку, визначтесь із сумою, яку ви готові витратити на авто. Введіть її в поле "Ціна до".
-            Потім, за бажанням, визначте додаткові параметри (об’єм двигуна, тощо). Через секунду графіки автоматично оновляться.
-            Дочекавшись завантаження, дивіться, які авто пропонує ринок під ваш запит.
-        </Typography>
+const Info = () => {
+    const [expanded, setExpanded] = useState<string | false>(false);
 
-        {/* {By Year} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Що показує графік "За роком викуску"?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Кількість активних оголошень, розподеліні за роком випуску.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Напівпрозорим кольором зображена загальна кількість оголошень на ринку, залежно від року випуску.
-            Повним кольором зображено кількість оголошень із застосуванням фільтрів.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Таким чином бачимо, авто якого року слід шукати.
-        </Typography>
+    const handleChange =
+        (panel: string) => (event: SyntheticEvent, isExpanded: boolean) => {
+            setExpanded(isExpanded ? panel : false);
+        };
 
-        {/* {By Region} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Що показує графік "За регіоном"?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Кількість активних оголошень, розподілені за областями.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Напівпрозорим кольором зображена загальна кількість оголошень на ринку.
-            Повним кольором зображено кількість оголошень із застосуванням фільтрів.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Таким чином бачимо, в яких регіонах найбільший вибір авто.
-        </Typography>
+    return (
+        <>
+            <Typography variant="h4" component="h2" gutterBottom sx={{ textAlign: 'center', mt: 2 }}>
+                Питання та Відповіді
+            </Typography>
 
-        {/* {By Brand} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Що показує графік "За брендом"?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Авто якої марки найбільше представлені на ринку, залежно від застосованих фільтрів.
-            Результати показують ТОП-25 за кількістю оголошень.
-        </Typography>
+            {/* {What is} */}
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary>
+                    <Typography>
+                        Що таке "autoRia Analyser?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Аналізатор - це інструмен, який візуально відображає авторинок України в реальному часі.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
 
-        {/* {Gearbox distribution} */}
-        <Typography variant="h6" component="h3" gutterBottom>
-            Навіщо потрібно "Показати розподілення АКПП до МКПП?
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            Авто старші за 10р. можуть мати зовсім мало пропозицій на АКПП.
-            В таких ситуаціях користо бачити розподілення на власні очі.
-        </Typography>
-    </>
-)
+            {/* {What for} */}
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary>
+                    <Typography>
+                        Для чого потрібен аналізатор?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Щоб в пару кліків зрозуміти, яке найліпше авто можна придбати в Україні, відштовхуючись від бюджету і побажань.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {How does it work} */}
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary>
+                    <Typography>
+                        Як працює аналізатор?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Аналізатор шукає оголошення на autoRia.ua, збирає статистику і будує інформативні графіки.
+                    </Typography>
+                    <Typography>
+                        Для початку, визначтесь із сумою, яку ви готові витратити на авто. Введіть її в поле "Ціна до".
+                        Потім, за бажанням, визначте додаткові параметри (об’єм двигуна, тощо). Через секунду графіки автоматично оновляться.
+                        Дочекавшись завантаження, дивіться, які авто пропонує ринок під ваш запит.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {By Year} */}
+            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary>
+                    <Typography>
+                        Що показує графік "За роком викуску"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Кількість активних оголошень, розподеліні за роком випуску.
+                    </Typography>
+                    <Typography>
+                        Напівпрозорим кольором зображена загальна кількість оголошень на ринку, залежно від року випуску.
+                        Повним кольором зображено кількість оголошень із застосуванням фільтрів.
+                    </Typography>
+                    <Typography>
+                        Таким чином бачимо, авто якого року слід шукати.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {By Region} */}
+            <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+                <AccordionSummary>
+                    <Typography >
+                        Що показує графік "За регіоном"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Кількість активних оголошень, розподілені за областями.
+                    </Typography>
+                    <Typography>
+                        Напівпрозорим кольором зображена загальна кількість оголошень на ринку.
+                        Повним кольором зображено кількість оголошень із застосуванням фільтрів.
+                    </Typography>
+                    <Typography>
+                        Таким чином бачимо, в яких регіонах найбільший вибір авто.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {By Brand} */}
+            <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+                <AccordionSummary>
+                    <Typography>
+                        Що показує графік "За брендом"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Авто якої марки найбільше представлені на ринку, залежно від застосованих фільтрів.
+                        Результати показують ТОП-25 за кількістю оголошень.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {Gearbox distribution} */}
+            <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+                <AccordionSummary>
+                    <Typography>
+                        Навіщо потрібно "Показати розподілення АКПП до МКПП?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Авто старші за 10р. можуть мати зовсім мало пропозицій на АКПП.
+                        В таких ситуаціях користо бачити розподілення на власні очі.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </>
+    )
+}
 
 export default Info;
