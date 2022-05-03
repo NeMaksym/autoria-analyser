8f06c4bc828d5dff3643ac62c7b8db291c4f6d22
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2022-05-03 14:20:13 +0300

    Update info

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,10 +1,14 @@
 import { useState, SyntheticEvent } from 'react';
 import { styled } from '@mui/material/styles';
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
+import {
+    Typography,
+    AccordionDetails as MuiAccordionDetails,
+    Accordion as MuiAccordion,
+    AccordionProps,
+    AccordionSummary as MuiAccordionSummary,
+    AccordionSummaryProps
+} from "@mui/material"
 
 const Accordion = styled((props: AccordionProps) => (
     <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -56,8 +60,12 @@ const Info = () => {
                 Питання та Відповіді
             </Typography>
 
+            <Typography variant="h6">
+                Загальні
+            </Typography>
+
             {/* {What is} */}
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <Accordion expanded={expanded === 'general1'} onChange={handleChange('general1')}>
                 <AccordionSummary>
                     <Typography>
                         Що таке "autoRia Analyser?
@@ -65,13 +73,13 @@ const Info = () => {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Аналізатор - це інструмен, який візуально відображає авторинок України в реальному часі.
+                        Аналізатор - це інструмент, який візуально зображає авторинок України в реальному часі.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
 
             {/* {What for} */}
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <Accordion expanded={expanded === 'general2'} onChange={handleChange('general2')}>
                 <AccordionSummary>
                     <Typography>
                         Для чого потрібен аналізатор?
@@ -79,42 +87,69 @@ const Info = () => {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Щоб в пару кліків зрозуміти, яке найліпше авто можна придбати в Україні, відштовхуючись від бюджету і побажань.
+                        Головна мета - це позбутися необхідності гортати autoria годинами. Замість цього, аналізатор за лічені хвилини підкаже
+                        на які саме авто слід дивитися.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
 
-            {/* {How does it work} */}
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            {/* {How} */}
+            <Accordion expanded={expanded === 'general3'} onChange={handleChange('general3')}>
                 <AccordionSummary>
                     <Typography>
                         Як працює аналізатор?
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Аналізатор шукає оголошення на autoRia.ua, збирає статистику і будує інформативні графіки.
+                    <Typography gutterBottom>
+                        1. Ви визначаєтесь з бажаними параметрами (бюджет, тип палива тощо) <br />
+                        2. Аналізатор шукає оголошення на autoRia.ua і збирає статистику <br />
+                        3. Аналізатор <i>показує</i> яке найліпше авто можна придбати
                     </Typography>
                     <Typography>
+                        Обирати найліпше авто можна самостійно за допомогою графіків або ж скориставшись алгоритмом автопідбору,
+                        який підкаже конкретну марку і модель.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Typography variant="h6" mt={1}>
+                Розділ "Графіки"
+            </Typography>
+
+            {/* {How does graphs work} */}
+            <Accordion expanded={expanded === 'graphs1'} onChange={handleChange('graphs1')}>
+                <AccordionSummary>
+                    <Typography>
+                        Як працює розділ "Графіки"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography gutterBottom>
+                        Аналізатор шукає оголошення на autoRia.ua, збирає статистику і будує інформативні графіки.
+                    </Typography>
+                    <Typography gutterBottom>
                         Для початку, визначтесь із сумою, яку ви готові витратити на авто. Введіть її в поле "Ціна до".
-                        Потім, за бажанням, визначте додаткові параметри (об’єм двигуна, тощо). Через секунду графіки автоматично оновляться.
-                        Дочекавшись завантаження, дивіться, які авто пропонує ринок під ваш запит.
+                        Потім, за бажанням, визначте додаткові параметри (об’єм двигуна тощо). Через секунду графіки почнуть оновлюватися.
+                    </Typography>
+                    <Typography>
+                        Далі справа за вами. Дивіться на графіках, які авто пропонує ринок під ваш запит. Обирайте найкраще.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
 
             {/* {By Year} */}
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <Accordion expanded={expanded === 'graphs2'} onChange={handleChange('graphs2')}>
                 <AccordionSummary>
                     <Typography>
                         Що показує графік "За роком викуску"?
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Кількість активних оголошень, розподеліні за роком випуску.
+                    <Typography gutterBottom>
+                        Кількість активних оголошень, розподілені за роком випуску.
                     </Typography>
-                    <Typography>
+                    <Typography gutterBottom>
                         Напівпрозорим кольором зображена загальна кількість оголошень на ринку, залежно від року випуску.
                         Повним кольором зображено кількість оголошень із застосуванням фільтрів.
                     </Typography>
@@ -125,17 +160,17 @@ const Info = () => {
             </Accordion>
 
             {/* {By Region} */}
-            <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+            <Accordion expanded={expanded === 'graphs3'} onChange={handleChange('graphs3')}>
                 <AccordionSummary>
                     <Typography >
                         Що показує графік "За регіоном"?
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                    <Typography gutterBottom>
                         Кількість активних оголошень, розподілені за областями.
                     </Typography>
-                    <Typography>
+                    <Typography gutterBottom>
                         Напівпрозорим кольором зображена загальна кількість оголошень на ринку.
                         Повним кольором зображено кількість оголошень із застосуванням фільтрів.
                     </Typography>
@@ -146,7 +181,7 @@ const Info = () => {
             </Accordion>
 
             {/* {By Brand} */}
-            <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+            <Accordion expanded={expanded === 'graphs4'} onChange={handleChange('graphs4')}>
                 <AccordionSummary>
                     <Typography>
                         Що показує графік "За брендом"?
@@ -160,17 +195,61 @@ const Info = () => {
                 </AccordionDetails>
             </Accordion>
 
-            {/* {Gearbox distribution} */}
-            <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+            {/* {By Model} */}
+            <Accordion expanded={expanded === 'graphs5'} onChange={handleChange('graphs5')}>
                 <AccordionSummary>
                     <Typography>
-                        Навіщо потрібно "Показати розподілення АКПП до МКПП?
+                        Що показує графік "За моделлю"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography gutterBottom>
+                        Авто якої моделі найбільше представлені на ринку.
+                    </Typography>
+                    <Typography>
+                        Щоб побачити цей графік, натисніть на стовпчик з брендом на графіку "За брендом".
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* {Gearbox distribution} */}
+            <Accordion expanded={expanded === 'graphs6'} onChange={handleChange('graphs6')}>
+                <AccordionSummary>
+                    <Typography>
+                        Навіщо потрібно "Показати співвідношення механіки та автомату"?
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Авто старші за 10р. можуть мати зовсім мало пропозицій на АКПП.
-                        В таких ситуаціях користо бачити розподілення на власні очі.
+                        Авто старші за 10 р. можуть мати зовсім мало пропозицій на автоматичній коробці.
+                        В таких ситуаціях корисно бачити розподілення на власні очі.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Typography variant="h6" mt={1}>
+                Розділ "Автопідбір"
+            </Typography>
+
+            {/* {How does carpicker work} */}
+            <Accordion expanded={expanded === 'carpicker1'} onChange={handleChange('carpicker1')}>
+                <AccordionSummary>
+                    <Typography>
+                        Як працює розділ "Автопідбір"?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography gutterBottom>
+                        1. Ви визначаєте параметри авто (бюджет, тип палива тощо)
+                        2. На цих параметрах аналізатор шукає найсвіжіші авто на ринку <br />
+                        3. Аналізатор залишає лише ті варіанти, за якими є достатня кількість пропозицій на ринку
+                    </Typography>
+                    <Typography gutterBottom>
+                        Тобто ви отримуєте перелік найсвіжіших авто, які можна придбати за ваш бюджет і які мають достатню кількість пропозицій на ринку.
+                    </Typography>
+                    <Typography>
+                        акий підхід економить ваш час, бо тепер непотрібно шукати моделі, яких немає на ринку.
+                        А також збільшує ваші шанси обрати нормальне авто, показуючи лише авто серед яких є достатній вибір.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
