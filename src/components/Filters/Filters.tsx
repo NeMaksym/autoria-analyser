ffc207e96caa55c409e3c4ef423db1adfda09cf7
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2022-05-07 16:04:40 +0300

    Improve filters on mobile view

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -29,14 +29,12 @@ const FUEL_LABELS = {
     6: 'Електо',
 }
 
-const WIDTH = 247;
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
     PaperProps: {
         style: {
             maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: WIDTH,
         },
     },
 };
@@ -50,52 +48,50 @@ interface Props {
 const Filters = ({ formik, compareGearboxes, setCompareGearboxes }: Props) => (
     <Paper sx={{ p: 4 }} elevation={4}>
         <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Grid container spacing={2}>
-                    <Grid item>
-                        <FormControl sx={{ width: 200 }}>
-                            <InputLabel shrink>Ціна до</InputLabel>
-                            <OutlinedInput
-                                notched
-                                label="Ціна до"
-                                type="number"
-                                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                value={formik.values.price_do}
-                                onChange={e => formik.setFieldValue('price_do', e.target.value)}
-                            />
-                        </FormControl>
-                    </Grid>
+            <Grid item xs={12} sm="auto">
+                <FormControl sx={{ width: { xs: '100%', sm: '200px' } }}>
+                    <InputLabel shrink>Ціна до</InputLabel>
+                    <OutlinedInput
+                        notched
+                        label="Ціна до"
+                        type="number"
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                        value={formik.values.price_do}
+                        onChange={e => formik.setFieldValue('price_do', e.target.value)}
+                    />
+                </FormControl>
+            </Grid>
 
-                    <Grid item>
-                        <FormControl sx={{ width: 100 }}>
-                            <InputLabel shrink>Двигун до</InputLabel>
-                            <OutlinedInput
-                                notched
-                                label="Двигун до"
-                                type="number"
-                                endAdornment={<InputAdornment position="end">л.</InputAdornment>}
-                                value={formik.values.engineVolumeTo}
-                                onChange={e => formik.setFieldValue('engineVolumeTo', e.target.value)}
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            sx={{ width: 100 }}
-                            label="Рік від"
+            <Grid item xs={12} sm="auto" container spacing={2}>
+                <Grid item>
+                    <FormControl sx={{ width: 100 }}>
+                        <InputLabel shrink>Двигун до</InputLabel>
+                        <OutlinedInput
+                            notched
+                            label="Двигун до"
                             type="number"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            value={formik.values['s_yers[0]']}
-                            onChange={e => formik.setFieldValue('s_yers[0]', e.target.value)}
+                            endAdornment={<InputAdornment position="end">л.</InputAdornment>}
+                            value={formik.values.engineVolumeTo}
+                            onChange={e => formik.setFieldValue('engineVolumeTo', e.target.value)}
                         />
-                    </Grid>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <TextField
+                        sx={{ width: 100 }}
+                        label="Рік від"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        value={formik.values['s_yers[0]']}
+                        onChange={e => formik.setFieldValue('s_yers[0]', e.target.value)}
+                    />
                 </Grid>
             </Grid>
 
             <Grid item xs={12}>
-                <FormControl sx={{ width: `${WIDTH}px` }}>
+                <FormControl sx={{ minWidth: { xs: "100%", sm: '315px' } }}>
                     <InputLabel>Тип палива</InputLabel>
                     <Select
                         multiple
